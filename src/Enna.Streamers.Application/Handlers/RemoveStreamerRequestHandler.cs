3600619b972a943aa6a7515a5c658c574e0f4bec
damianfadri <55902230+damianfadri@users.noreply.github.com>
2023-02-27 00:31:21 +0800

    feat: Add remove streamer interaction (#3)
    * add remove streamer
    
    * remove whitespace

---
+++ src/Enna.Streamers.Application/Handlers/RemoveStreamerRequestHandler.cs
@@ -0,0 +1,42 @@
+﻿using Enna.Streamers.Application.Contracts;
+using Enna.Streamers.Domain;
+using Enna.Streamers.Domain.SeedWork;
+using MediatR;
+
+namespace Enna.Streamers.Application.Handlers
+{
+    public class RemoveStreamerRequestHandler
+        : IRequestHandler<RemoveStreamerRequest>
+    {
+        private readonly IStreamerRepository _streamerRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public RemoveStreamerRequestHandler(
+            IStreamerRepository streamerRepository,
+            IUnitOfWork unitOfWork) 
+        {
+            ArgumentNullException.ThrowIfNull(streamerRepository);
+            ArgumentNullException.ThrowIfNull(unitOfWork);
+
+            _streamerRepository = streamerRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task Handle(
+            RemoveStreamerRequest request, 
+            CancellationToken cancellationToken)
+        {
+            var streamer = await _streamerRepository
+                .FindById(request.StreamerId);
+
+            if (streamer == null)
+            {
+                throw new InvalidOperationException(
+                    $"Streamer id '{request.StreamerId}' does not exist.");
+            }
+
+            await _streamerRepository.Remove(streamer);
+            await _unitOfWork.CommitAsync();
+        }
+    }
+}
---
+++ test/Enna.Streamers.Application.Tests.Unit/RemoveStreamerRequestHandlerTests.cs
@@ -0,0 +1,73 @@
+﻿using Enna.Streamers.Application.Contracts;
+using Enna.Streamers.Domain;
+using FluentAssertions;
+using Xunit;
+
+namespace Enna.Streamers.Application.Tests.Unit
+{
+    public class RemoveStreamerRequestHandlerTests
+    {
+        public class Constructor_Should
+        {
+            [Fact]
+            public void ThrowException_When_StreamerRepositoryIsNull()
+            {
+                var sut = () =>
+                    new RemoveStreamerRequestHandlerSutBuilder()
+                        .WithNullStreamerRepository()
+                        .Build();
+
+                sut.Should().Throw<ArgumentNullException>();
+            }
+
+            [Fact]
+            public void ThrowException_When_UnitOfWorkIsNull()
+            {
+                var sut = () =>
+                    new RemoveStreamerRequestHandlerSutBuilder()
+                        .WithNullUnitOfWork()
+                        .Build();
+
+                sut.Should().Throw<ArgumentNullException>();
+            }
+        }
+
+        public class Handle_Should
+        {
+            [Fact]
+            public async Task ThrowException_When_StreamerIsNotFound()
+            {
+                var streamerId = Guid.NewGuid();
+
+                var handler =
+                    new RemoveStreamerRequestHandlerSutBuilder()
+                        .WithMissingStreamer(streamerId)
+                        .Build();
+
+                var sut = () => 
+                    handler.Handle(
+                        new RemoveStreamerRequest(streamerId),
+                        CancellationToken.None);
+
+                await sut.Should().ThrowAsync<InvalidOperationException>();
+            }
+
+            [Fact]
+            public async Task RemoveStreamerFromDatabase()
+            {
+                var streamer = new Streamer(
+                    Guid.NewGuid(),
+                    "Streamer name");
+
+                var handler =
+                    new RemoveStreamerRequestHandlerSutBuilder()
+                        .WithExistingStreamer(streamer)
+                        .Build();
+
+                await handler.Handle(
+                    new RemoveStreamerRequest(streamer.Id),
+                    CancellationToken.None);
+            }
+        }
+    }
+}
