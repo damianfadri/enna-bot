9b04b90de2e4e02991d271682225df918be6c506
damianfadri <damianfadri@gmail.com>
2023-03-01 18:59:03 +0800

    add tests

---
+++ test/Enna.Discord.Application.Tests.Unit/GetTextChannelFeedRequestHandlerSutBuilder.cs
@@ -0,0 +1,47 @@
+﻿using Enna.Discord.Application.Handlers;
+using Enna.Discord.Domain;
+using Enna.Streamers.Domain;
+using Moq;
+
+namespace Enna.Discord.Application.Tests.Unit
+{
+    public class GetTextChannelFeedRequestHandlerSutBuilder
+    {
+        private ITextChannelFeedRepository _textChannelRepository;
+
+        public GetTextChannelFeedRequestHandlerSutBuilder()
+        {
+            _textChannelRepository = new Mock<ITextChannelFeedRepository>().Object;
+        }
+
+        public GetTextChannelFeedRequestHandlerSutBuilder WithNullTextChannelRepository()
+        {
+            _textChannelRepository = null!;
+            return this;
+        }
+
+        public GetTextChannelFeedRequestHandlerSutBuilder WithMissingFeed(Guid feedId)
+        {
+            Mock.Get(_textChannelRepository)
+                .Setup(repository => repository.FindById(feedId))
+                .ReturnsAsync((TextChannelFeed)null!);
+
+            return this;
+        }
+
+        public GetTextChannelFeedRequestHandlerSutBuilder WithExistingFeed(TextChannelFeed feed)
+        {
+            Mock.Get(_textChannelRepository)
+                .Setup(repository => repository.FindById(feed.Id))
+                .ReturnsAsync(feed);
+
+            return this;
+        }
+
+        public GetTextChannelFeedRequestHandler Build()
+        {
+            return new GetTextChannelFeedRequestHandler(
+                _textChannelRepository);
+        }
+    }
+}
