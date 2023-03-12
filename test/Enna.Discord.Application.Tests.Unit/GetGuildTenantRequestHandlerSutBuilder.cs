b5e5962ad5400d3ee66f3129d1dcba6edcdcbe80
damianfadri <damianfadri@gmail.com>
2023-03-12 13:52:07 +0800

    get guild tenant

---
+++ test/Enna.Discord.Application.Tests.Unit/GetGuildTenantRequestHandlerSutBuilder.cs
@@ -0,0 +1,46 @@
+﻿using Enna.Discord.Application.Handlers;
+using Enna.Discord.Domain;
+using Enna.Streamers.Domain;
+using Moq;
+
+namespace Enna.Discord.Application.Tests.Unit
+{
+    public class GetGuildTenantRequestHandlerSutBuilder
+    {
+        private IGuildTenantRepository _guildTenantRepository;
+
+        public GetGuildTenantRequestHandlerSutBuilder()
+        {
+            _guildTenantRepository = new Mock<IGuildTenantRepository>().Object;
+        }
+
+        public GetGuildTenantRequestHandlerSutBuilder WithNullGuildTenantRepository()
+        {
+            _guildTenantRepository = null!;
+            return this;
+        }
+
+        public GetGuildTenantRequestHandlerSutBuilder WithMissingTenant(ulong guildId)
+        {
+            Mock.Get(_guildTenantRepository)
+                .Setup(repository => repository.FindByGuildId(guildId))
+                .ReturnsAsync((Tenant<ulong>)null!);
+
+            return this;
+        }
+
+        public GetGuildTenantRequestHandlerSutBuilder WithExistingTenant(Tenant<ulong> tenant)
+        {
+            Mock.Get(_guildTenantRepository)
+                .Setup(repository => repository.FindByGuildId(tenant.KeyId))
+                .ReturnsAsync(tenant);
+
+            return this;
+        }
+
+        public GetGuildTenantRequestHandler Build()
+        {
+            return new GetGuildTenantRequestHandler(_guildTenantRepository);
+        }
+    }
+}
---
+++ test/Enna.Discord.Application.Tests.Unit/GetGuildTenantRequestHandlerTests.cs
@@ -0,0 +1,65 @@
+﻿using Enna.Discord.Application.Contracts;
+using Enna.Streamers.Domain;
+using FluentAssertions;
+using Xunit;
+
+namespace Enna.Discord.Application.Tests.Unit
+{
+    public class GetGuildTenantRequestHandlerTests
+    {
+        public class Constructor_Should
+        {
+            [Fact]
+            public void ThrowException_When_TenantRepositoryIsNull()
+            {
+                var sut = () =>
+                    new GetGuildTenantRequestHandlerSutBuilder()
+                        .WithNullGuildTenantRepository()
+                        .Build();
+
+                sut.Should().Throw<ArgumentNullException>();
+            }
+        }
+
+        public class Handle_Should 
+        {
+            [Fact]
+            public async Task ThrowException_When_TenantIsNotFound()
+            {
+                var handler
+                    = new GetGuildTenantRequestHandlerSutBuilder()
+                        .WithMissingTenant(1L)
+                        .Build();
+
+                var sut = () =>
+                    handler.Handle(
+                        new GetGuildTenantRequest(1L),
+                        CancellationToken.None);
+
+                await sut.Should().ThrowAsync<InvalidOperationException>();
+            }
+
+            [Fact]
+            public async Task ReturnTenantDto()
+            {
+                var tenant
+                    = new Tenant<ulong>(
+                        Guid.NewGuid(), 
+                        1L);
+
+                var handler
+                    = new GetGuildTenantRequestHandlerSutBuilder()
+                        .WithExistingTenant(tenant)
+                        .Build();
+
+                var dto
+                    = await handler.Handle(
+                        new GetGuildTenantRequest(1L),
+                        CancellationToken.None);
+
+                dto.Id.Should().Be(tenant.Id);
+                dto.GuildId.Should().Be(tenant.KeyId);
+            }
+        }
+    }
+}
